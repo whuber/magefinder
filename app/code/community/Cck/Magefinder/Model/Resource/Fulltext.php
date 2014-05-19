c1fb177d3363a3c59ad9170e9051a1549c1e4c54
Ahe <info@andre-herrn.de>
2014-05-19 13:08:26 +0200

    #4: Added check for deleted products

--- app/code/community/Cck/Magefinder/Model/Resource/Fulltext.php
+++ app/code/community/Cck/Magefinder/Model/Resource/Fulltext.php
@@ -24,11 +24,15 @@ class Cck_Magefinder_Model_Resource_Fulltext extends Mage_CatalogSearch_Model_Re
         if (!$query->getIsProcessed()) {
 
             $result = $this->_getSearchAdapter()->query($queryText, (int)$query->getStoreId());
+
             $data = array();
             foreach ($result as $item) {
                 $item['query_id'] = (int)$query->getId();
                 $data[] = $item;
             }
+
+            $data = $this->_removeDeletedProducts($data);
+            
             if($data) {
                 $adapter->insertOnDuplicate(
                     $this->getTable('catalogsearch/result'),
@@ -50,5 +54,41 @@ class Cck_Magefinder_Model_Resource_Fulltext extends Mage_CatalogSearch_Model_Re
     protected function _getSearchAdapter()
     {
         return Mage::getResourceSingleton('magefinder/magefinder');
+    }
+
+    /**
+     * Remove deleted products from search result to avoid foreign key error
+     * 
+     * @param array $searchResults
+     * @return array
+     */
+    protected function _removeDeletedProducts($searchResults)
+    {
+        //Parse product ids
+        $productIdsToCheck = array();
+        foreach ($searchResults as $searchResult) {
+            $productIdsToCheck[] = $searchResult['product_id'];
+        }
+
+        //Get all existing products
+        $productCollectionOfExistingProducts = Mage::getResourceModel('catalog/product_collection');
+        $productCollectionOfExistingProducts
+            ->addFieldToFilter(
+                'entity_id',
+                array('in'=>$productIdsToCheck)
+            )
+            ->load();
+
+        //Get removed products
+        $deletedProductIds = array_diff($productIdsToCheck, $productCollectionOfExistingProducts->getAllIds());
+
+        //Remove deleted products from search result
+        foreach ($searchResults as $key => $searchResult) {
+            if (true === in_array($searchResult['product_id'], $deletedProductIds)) {
+                unset($searchResults[$key]);
+            }
+        }
+
+        return $searchResults;
     }
 }
