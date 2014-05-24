ee4d33cd244f7e03f8cea520d14785bb77932854
Walter Huber <walter@flimmit.com>
2014-05-24 08:05:58 +0200

    Autosuggest redirect on dedicated product id

--- app/code/community/Cck/Magefinder/Model/Observer.php
+++ app/code/community/Cck/Magefinder/Model/Observer.php
@@ -22,27 +22,6 @@ class Cck_Magefinder_Model_Observer
         if($id) {
             $product = Mage::getModel('catalog/product')->load($id);
             if($product->isInStock()) {
-                $forward = new Mage_Core_Controller_Varien_Exception();
-                $forward->prepareForward(
-                        'view', 
-                        'product', 
-                        'catalog', 
-                        array('id' => $id)
-                );
-                throw $forward;
-            }
-        }
-        return $this;
-    }
-
-    public function __redirectProductSearch(Varien_Event_Observer $observer)
-    {
-        $action = $observer->getControllerAction();
-        $query  = $action->getRequest()->getParam('q');
-        $id     = preg_match("%^product:([0-9]*)$%", $query, $match)?(int)$match[1]:false;
-        if($id) {
-            $product = Mage::getModel('catalog/product')->load($id);
-            if($product->isInStock()) {
                 $url = Mage::helper('catalog/product')->getProductUrl($product);
                 $action->setFlag('', Mage_Core_Controller_Varien_Action::FLAG_NO_DISPATCH, true);
                 $action->getResponse()->setRedirect($url);
